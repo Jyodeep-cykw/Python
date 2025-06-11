168b005381686ad11ae38aaaa6524e4af7c4ed39
Jyodeep <jyodeep@Jyodeeps-MacBook-Air.local>
2025-06-10 22:01:44 -0400

    Bank acess

---
+++ Federal_Bank.py
@@ -0,0 +1,14 @@
+
+user = input("Greeting: ").lower().strip()
+
+
+
+
+if (user[0] == 'h'):
+    if 'hello' in user:
+        print("$0")
+    else:
+        print("$20")
+    
+if(user[0]!= 'h'):
+    print("$100")
