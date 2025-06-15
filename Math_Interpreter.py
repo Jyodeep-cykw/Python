ae6d3d9ac660c1acd193fb8f9517ffdef6aa30ef
Jyodeep <jyodeep@Jyodeeps-MacBook-Air.local>
2025-06-15 13:40:44 -0400

    This takes an expression from the user and returns the answers

---
+++ Math_Interpreter.py
@@ -0,0 +1,18 @@
+expression = input("Expression: ")
+
+x, y, z = expression.split(" ")
+
+
+new_x = float(x)
+new_z = float(z)
+
+if y == "+":
+    print(new_x + new_z)
+elif y == "-":
+    print(new_x - new_z)
+elif y == "/":
+    print(new_x / new_z)
+elif y == "*":
+    print(new_x * new_z)
+else:
+    print("Invalid Expression")
