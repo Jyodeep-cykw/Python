48b7399aafe52cc64bff424f88483568a3944693
Jyodeep <jyodeep@Jyodeeps-MacBook-Air.local>
2025-06-16 17:09:39 -0400

    This code will remove all the vowels in the sentence or word

---
+++ Twitter.py
@@ -0,0 +1,18 @@
+
+
+user = input("Input: ").lower()
+
+print("Output: ", end="")
+
+for i in user:
+    if not i in ["a", "e", "i", "o", "u"]:
+        print(i, end = "")
+
+print()
+   
+
+    
+            
+
+
+    
