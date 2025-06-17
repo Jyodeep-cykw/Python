323409c94edd677dab33ea35b9130aa03ab1f78c
Jyodeep <jyodeep@Jyodeeps-MacBook-Air.local>
2025-06-17 11:48:33 -0400

    Shows the calories based on the item that it inputted. If the item is not found it ignores it.

---
+++ Nutrition_Facts.py
@@ -0,0 +1,32 @@
+Nutrition = {
+    
+    "apple": 130,
+    "avacodo": 50,
+    "banana" : 110,
+    "cantaloupe" : 50,
+    "grapefruit" : 60,
+    "grapes" : 90,
+    "honeydew melon": 50,
+    "kiwifruit" : 90,
+    "lemon" : 15,
+    "lime" : 20,
+    "nectarine" : 60,
+    "orange" : 80,
+    "peach" : 60,
+    "pear": 100,
+    "pineapple" : 50,
+    "plums" : 70,
+    "strawberries" : 50,
+    "sweet cherries": 100,
+    "tangerine" : 50,
+    "watermelon" : 80    
+}
+
+
+user = input("Item: ").lower()
+
+for fruit in Nutrition:
+    if (user == fruit):
+        print("Calories: " + str(Nutrition[fruit]))
+        break
+    
